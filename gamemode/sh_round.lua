d70e0b33acb69c666ea38158583f5385cf153a75
AwfulRanger <AwfulRanger@users.noreply.github.com>
2018-06-04 05:18:19 -0400

    Change ROUND_ERROR to ROUND_INITIALIZING, set RoundState

--- gamemode/sh_round.lua
+++ gamemode/sh_round.lua
@@ -5,11 +5,13 @@ DEFINE_BASECLASS( "gamemode_base" )
 ----
 --Set/get round state
 ----
-ROUND_ERROR = -1
+ROUND_INITIALIZING = -1
 ROUND_INTERMISSION = 0
 ROUND_STARTING = 1
 ROUND_INPROGRESS = 2
 ROUND_ENDING = 3
+
+GM.RoundState = ROUND_INITIALIZING
 
 function GM:SetRoundState( state )
 	
