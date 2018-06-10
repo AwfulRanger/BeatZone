8925d9ee75555d0f0edb477e6fa40387393c6593
AwfulRanger <AwfulRanger@users.noreply.github.com>
2018-06-10 04:00:54 -0400

    Add team for players

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -6,3 +6,11 @@ DEFINE_BASECLASS( "gamemode_base" )
 
 GM.Name = "BeatZone"
 GM.Author = "AwfulRanger"
+GM.TeamBased = true
+
+TEAM_BEAT = 1
+function GM:CreateTeams()
+	
+	team.SetUp( TEAM_BEAT, "Beat", Color( 0, 255, 0 ) )
+	
+end
